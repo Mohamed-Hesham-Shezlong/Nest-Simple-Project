7301b39ff17a1259a16be65e3f0a739fe2e47c80
mohamed hesham <scaret2tv@gmail.com>
2025-07-16 14:30:08 +0300

    Added 'api/v1' prefix to all endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   const configService = app.get(ConfigService);
   const port = configService.get<number>('server.port') as number;
   await app.listen(port);
