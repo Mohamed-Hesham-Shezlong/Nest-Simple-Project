eedd41518587d5f2f60e549fb6afb8a3e0be643a
mohamed hesham <scaret2tv@gmail.com>
2025-07-17 14:42:59 +0300

    update the therapists contoller to check if admin for creating and updating therapists

--- src/therapists/therapists.controller.ts
+++ src/therapists/therapists.controller.ts
@@ -2,18 +2,22 @@ import {
   Controller,
   Get,
   Post,
-  Delete,
   Body,
   Param,
+  Delete,
   UseGuards,
+  UnauthorizedException,
   HttpException,
   HttpStatus,
   Patch,
 } from '@nestjs/common';
 import { TherapistsService } from './therapists.service';
 import { CreateTherapistDto } from './dto/create-therapist.dto';
-import { Therapist } from './interfaces/therapist.interface';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { User } from '../auth/entities/user.entity';
+import { Therapist } from './entities/therapist.entity';
+import { UserRole } from 'src/auth/interfaces/user.role';
 
 @Controller('therapists')
 @UseGuards(JwtAuthGuard)
@@ -23,7 +27,12 @@ export class TherapistsController {
   @Post()
   async create(
     @Body() createTherapistDto: CreateTherapistDto,
-  ): Promise<Therapist> {
+    @CurrentUser() user: User,
+  ) {
+    // Only admin users can create therapists
+    if (user.role !== UserRole.ADMIN) {
+      throw new UnauthorizedException('Only admin users can create therapists');
+    }
     return await this.therapistsService.create(createTherapistDto);
   }
 
@@ -44,23 +53,22 @@ export class TherapistsController {
   @Patch(':id')
   async update(
     @Param('id') id: string,
-    @Body() updateData: Partial<CreateTherapistDto>,
-  ): Promise<Therapist> {
-    const therapist = await this.therapistsService.update(
-      Number(id),
-      updateData,
-    );
-    if (!therapist) {
-      throw new HttpException('Therapist not found', HttpStatus.NOT_FOUND);
+    @Body() updateTherapistDto: Partial<CreateTherapistDto>,
+    @CurrentUser() user: User,
+  ) {
+    // Only admin users can update therapists
+    if (user.role !== UserRole.ADMIN) {
+      throw new UnauthorizedException('Only admin users can update therapists');
     }
-    return therapist;
+    return this.therapistsService.update(+id, updateTherapistDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<void> {
-    const isDeleted = await this.therapistsService.remove(Number(id));
-    if (!isDeleted) {
-      throw new HttpException('Therapist not found', HttpStatus.NOT_FOUND);
+  async remove(@Param('id') id: string, @CurrentUser() user: User) {
+    // Only admin users can delete therapists
+    if (user.role !== UserRole.ADMIN) {
+      throw new UnauthorizedException('Only admin users can delete therapists');
     }
+    return await this.therapistsService.remove(+id);
   }
 }
